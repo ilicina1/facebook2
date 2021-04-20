bfa2a123683943a840d0a466da1cdfc031e02171
ilicina1 <ilhan.licina@outlook.com>
2021-04-20 16:22:24 +0200

    dovrsen signInPage

--- lib/view/IogInView/pages/signInScreen.dart
+++ lib/view/IogInView/pages/signInScreen.dart
@@ -1,4 +1,5 @@
 import 'package:facebook_2/services/signInFacebook.dart';
+import 'package:facebook_2/view/IogInView/widgets/facebookLogo.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -8,32 +9,49 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[900],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SafeArea(child: Text("")),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () {
-                  // signInWithFacebook();
-                  initiateFacebookLogin();
-                },
-                child: Text(
-                  "SignIn",
-                  style: TextStyle(
-                    color: Colors.white,
-                    backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            FaceBookLogo(),
+            TextButton(
+              onPressed: () {
+                initiateFacebookLogin();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "SignIn",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                      ),
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ],
+                  Icon(
+                    Icons.login,
+                    color: Colors.blue[900],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
