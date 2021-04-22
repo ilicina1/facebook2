412b14004a8b337528bd059fb9989b5bd92c057c
amer460 <amer.zlatic.az@gmail.com>
2021-04-22 14:32:11 +0200

    stavljeno u funkciju lajkova i dislajkovanje

--- lib/view/mainPage/widgets/like_dislike_button.dart
+++ lib/view/mainPage/widgets/like_dislike_button.dart
@@ -2,22 +2,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../../services/main_screen_services/add_post.dart';
+
 Widget likeDislikeButton(
     bool likeOrDislike, document, CollectionReference collection, int index) {
   return OutlinedButton(
     // width: MediaQuery.of(context).size.width * 0.45,
     onPressed: () {
       //document['likes'] = 10;
-      //FirebaseFirestore.instance.collection("posts").doc(document).set({'likes': 20});
-      collection.doc('$index').set({
-        'created': document['created'],
-        'dislikes': document['dislikes'],
-        'image': document['image'],
-        'likes': 487,
-        'name': document['name'],
-        'postText': document['postText'],
-        'userMail': document['userMail'],
-      });
+      if (likeOrDislike) {
+        FirebaseFirestore.instance.collection("posts").doc(document.id).set({
+          'created': document['created'],
+          'dislikes': document['dislikes'],
+          'image': document['image'],
+          'likes': (document['likes'] + 1),
+          'name': document['name'],
+          'postText': document['postText'],
+          'userMail': document['userMail'],
+        });
+      } else {
+        FirebaseFirestore.instance.collection("posts").doc(document.id).set({
+          'created': document['created'],
+          'dislikes': (document['dislikes'] + 1),
+          'image': document['image'],
+          'likes': document['likes'],
+          'name': document['name'],
+          'postText': document['postText'],
+          'userMail': document['userMail'],
+        });
+      }
+
+      //addPost('name', 'image', 'postText', 'mail');
 
       //Firestore.instance.document('Users/$uid');
     },
