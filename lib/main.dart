5185878f7522bbfc20f0dd1ae70a6a5ce040691d
ajdann <ajding98@gmail.com>
2021-04-25 13:18:34 +0200

    added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:facebook_2/utils/style/style.dart';
+import 'package:facebook_2/view/IogInView/pages/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 2,
+        navigateAfterSeconds: new LogInScreen(),
+        title: new Text(
+          'Welcome To Facebook',
+          style: splashStyle,
+        ),
+        image: new Image.asset("assets/images/facebook.png"),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.blue[900]);
+  }
+}
